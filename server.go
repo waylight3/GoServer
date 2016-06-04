b41faf3eb9840e5e3985c283fee7231dc36c9f02
waylight3 <waylight3@naver.com>
2016-06-05 00:19:25 +0900

    create go file

---
+++ server.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) { w.Write([]byte("Hello World")) })
+	http.ListenAndServe(":5000", nil)
+}
